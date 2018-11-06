de99186d9ec9c2df73f6a0fe3e77054ee63e6aa7
gitinho <thug6757@gmail.com>
2018-11-06 12:22:43 +0000

    hashing passwords

--- action_login.php
+++ action_login.php
@@ -1,8 +1,11 @@
 <?php
 include_once('includes/init.php');
 include_once('database/user.php');
+hash_password('Ben1', '123');
+hash_password('Ben2', '234');
+hash_password('Ben3', '345');
 
-  if (isLoginCorrect($_POST['username'], $_POST['password'])) {
+  if (isLoginCorrect($_POST['username'], md5($_POST['password']))) {
     //setCurrentUser($_POST['username']);
     echo "<script type='text/javascript'>alert('Done it!');</script>";
     //$_SESSION['success_messages'][] = "Login Successful!";
--- database/user.php
+++ database/user.php
@@ -16,8 +16,16 @@
 
     function addNewUser($username, $password) {
         global $dbh;
-        $stmt = $dbh->prepare("INSERT INTO users (username, password) VALUES('$username', '$password');");
+        $stmt = $dbh->prepare("INSERT INTO users VALUES (?, ?);");
         $stmt->execute();
+        return $stmt->fetch() !== false;
+    }
+
+    function hash_password($username, $password) {
+        global $dbh;
+        $hashed_password = md5($password);
+        $stmt = $dbh->prepare("UPDATE users SET password = ? WHERE username = ?");
+        $stmt->execute(array($hashed_password, $username));
         return $stmt->fetch() !== false;
     }
 
