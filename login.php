764e034b32d289b582bed7a6ec39e6997f920242
Lucia Konštiaková <konstiakova6@stud.uniza.sk>
2018-12-11 10:48:59 +0000

    adding main.css (not finished)

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Test</title>
+    <title>Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="main.js"></script>
@@ -11,7 +11,9 @@
 <body>
 
     <form action="action_login.php" method="post">
+      <label for="username">Username</label><br>
       <input type="text" placeholder="username" name="username">
+      <label for="password">Password</label><br>
       <input type="password" placeholder="password" name="password">
       <div>
         <input type="submit" value="Login">
--- register.php
+++ register.php
@@ -1,28 +1,43 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <title> Register </title>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Test</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="main.js"></script>
 </head>
 <body>
-
+    <div id="register">
     <h2>Register</h2>
     <form action="action_register.php" method="post">
+        <p>
+        <label for="username">Username</label><br>
+        <div class="tooltip">
         <input type="text" name="username" placeholder="Username" required>
-        <span class="hint">Only lowercase, at least 3 characters</span>
-
+        <span class="tooltiptext">Only lowercase, at least 3 characters</span>
+        </div>    
+        </p>
+        <p>
+        <label for="password">Password</label><br>
+        <div class="tooltip">
         <input type="password" name="password" placeholder="Password" required>
-        <span class="hint">One uppercase, 1 symbol, 1 number, at least 8 characters</span>
-
+        <span class="tooltiptext">One uppercase, 1 symbol, 1 number, at least 8 characters</span>
+        </div>
+        </p>    
+        <p>
+        <label for="password">Repeat Password</label><br>
+        <div class="tooltip">
         <input type="password" name="repeat" placeholder="Repeat Password" required>
-        <span class="hint">Must match password</span>
-
+        <span class="tooltiptext">Password must match</span>
+        </div>
+        </p>
+        <p class="register_submit">
         <input type="submit" value="Register">
+        </p>
     </form>
+    </div>
 
 </body>
 </html>
