126d09430361cc7dae894e2751e3a40aed07f248
Lucia Konštiaková <konstiakova6@stud.uniza.sk>
2018-12-17 23:44:17 +0000

    registration_validation

--- index.php
+++ index.php
@@ -15,8 +15,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" media="screen" href="layout.css" />
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="style/layout.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="style/main.css" />
 
     <title>Document</title>
 </head>
